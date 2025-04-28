4d166c883bc9be2903d36e958dd09e4369b7a562
padmanabhan08 <padmanabhan123345@gmail.com>
2025-04-28 18:37:56 +0530

    promise

--- src/app/(rs)/customers/form/page.tsx
+++ src/app/(rs)/customers/form/page.tsx
@@ -1,51 +1,51 @@
-// import { getCustomer } from "@/lib/queries/getCustomer";
+import { getCustomer } from "@/lib/queries/getCustomer";
 
-// export default async function GetCustomerForm({
-//   searchParams,
-// }: {
-//   searchParams: { [key: string]: string | undefined };
-// }) {
-//   try {
-//     const params = await searchParams; // <-- await because now it's a Promise
+export default async function GetCustomerForm({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+  try {
+    const params = await searchParams; // <-- await because now it's a Promise
 
-//     console.log('Search Params:', params);
+    console.log('Search Params:', params);
 
-//     const customerId = params.customerId;
-//     console.log('Customer ID:', customerId);
+    const customerId = params.customerId;
+    console.log('Customer ID:', customerId);
 
-//     if (!customerId) {
-//       return <div>Customer ID is missing</div>;
-//     }
+    if (!customerId) {
+      return <div>Customer ID is missing</div>;
+    }
 
-//     const customer = await getCustomer(Number(customerId));
-//     console.log('Customer:', customer);
+    const customer = await getCustomer(Number(customerId));
+    console.log('Customer:', customer);
 
-//     if (!customer) {
-//       return <div>Customer not found</div>;
-//     }
+    if (!customer) {
+      return <div>Customer not found</div>;
+    }
 
-//     return (
-//       <div>
-//         <h1>Customer Form</h1>
-//         <form>
-//           <label>
-//             First Name:
-//             <input type="text" name="fname"  />
-//           </label>
-//           <label>
-//             Last Name:
-//             <input type="text" name="lname" />
-//           </label>
-//           <label>
-//             Phone:
-//             <input type="text" name="phone" />
-//           </label>
-//           <button type="submit">Update</button>
-//         </form>
-//       </div>
-//     );
-//   } catch (error) {
-//     console.error("Error fetching customer form:", error);
-//     return <div>Error fetching customer form</div>;
-//   }
-// }
+    return (
+      <div>
+        <h1>Customer Form</h1>
+        <form>
+          <label>
+            First Name:
+            <input type="text" name="fname"  />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" name="lname" />
+          </label>
+          <label>
+            Phone:
+            <input type="text" name="phone" />
+          </label>
+          <button type="submit">Update</button>
+        </form>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching customer form:", error);
+    return <div>Error fetching customer form</div>;
+  }
+}
